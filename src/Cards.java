6b019a68c64a3c664fcdac6be17e0bcc3d2cf989
Benjamin Clarke <benjiclarke@me.com>
2017-02-22 18:38:09 +0000

    Initial commit

---
+++ src/Cards.java
@@ -0,0 +1,9 @@
+/**
+ * Created by benjaminclarke on 22/02/2017.
+ */
+public class Cards {
+    public static void main(String[] args){
+
+        System.out.println("hello World!");
+    }
+}
---
+++ src/Dealer.java
@@ -0,0 +1,12 @@
+/**
+ * Created by benjaminclarke on 22/02/2017.
+ */
+public interface Dealer {
+
+    void shuffle(Deck d);
+
+    void search(Deck d);
+
+    void sort(Deck d);
+
+}
